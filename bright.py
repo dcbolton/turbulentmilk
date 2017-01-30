08ba834476ccc6e1d93c0e1e0a5ff1c3da23c767
Dempsey <dcbolton@ualberta.ca>
2017-01-30 18:09:20 +0000

    updated

--- bright.py
+++ bright.py
@@ -9,12 +9,11 @@ def func(argv):
         high = unravel_index(s.argmax(), s.shape)
 	plt.plot(s[:,high[1],high[2]].value)
 	figname = argv[:-5] + ' ' + str(high[1]) + ' ' + str(high[2])
-	print figname
+	
 	plt.savefig(figname)
 	plt.clf()
-	print('func')
+	
 
-print('Before')
 import glob
 array = glob.glob("/mnt/work/dcbolton/top10/*.fits")
 i=0
@@ -25,5 +24,4 @@ for name in array:
 	i=i+1
 	#func(sys.argv[1])
 
-print('After')
 
